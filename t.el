2a7b4954ae575831cee1f3d0ef3cecd6ec3d55c3
jason_he <jason_he@me.com>
2014-09-09 10:23:40 +0800

    update
    t.el

--- t.el
+++ t.el
@@ -1,101 +1,84 @@
-;; emacs lisp 学习
+(float-time (current-time))
 
-;; defvar 
-;; 事先声明变量，好处是可以定义变量的初始值以及文档
-(defvar hx-var1 (concat "var1" "/" "xx") "def var 变量文档")
+;; string matching function
+(string-match "=" "HOME=home1")
+(match-beginning 0)
+(match-end 0)
+(match-data)
 
-;; defgroup
-;; 定制选项
-;; 定制项的名称， 定制的方式，所属的group
-(defgroup hx-practice 
-  '((var1 custom-varible))
-  "defgroup 练习"
-  :group 'environment)
-
-;; defcustom
-
-(defcustom hx-custom-choice1 nil
-  "定制选项1"  
-  :type '(choice
-	  (const :tag "X1" nil)
-	  (file  :tag "文件" :value "~/.emacs"))
-  :version "23.1"
-  :group 'hx-practice)
-
-
-(defvaralias 'hx-c-c1 'hx-custom-choice1)
-
-;; 以下是启动时设置的hook
-
-(defun hx-hook1 ()
-  (message "hello"))
-
-
-
-;; 搞清楚以下的几个 hook 运行的时机、区别以及应用
-;; before-init-hook
-(add-hook 'after-init-hook 'hx-hook)
-
-;; after-init-hook
-
-;; emacs-starup-hook
-
-;; term-setup-hook
-
-
-;; window-setup-hook
-
-
-;; inhibit-startup-hooks
-;; inhibit-default0init
-
-;; emacs 启动时加载的文件顺序
-;; emacs's dumped image 
-;; TODO: 搞清楚 dumped image 的意义 
-;; site-run-file 默认为 site-start.el
-;; .emacs or init.el
-;; default.el
-
-;; preloaded & autoloaded 这两种情况
-
-;; 对于运行时来说，代码的加载有两种情况
-;; 核心的代码在启动时就加载执行
-;; 一般的功能代码在被调用时再自动加载
-
-
-;; 创建一个 buffer，并对该 buffer 进行一些编辑
 (with-current-buffer (get-buffer-create "*hx-test*")
-  ;; (point-min)
-  (erase-buffer)
-  ;; (org-mode)
-  (insert "\n")
-  (insert "---------------")
-  (insert "伟大的编辑器")  
-  (insert "---------------")
-  (insert "\n"))
+  (let* ((f1 '(lambda (s) (format "entered: %s\n" s))))
+    (erase-buffer)
+    (insert "========== NOTE ==========")
+    (insert "\n")
+    (insert (format "length of the undo list: %s\n" (length buffer-undo-list)))
+    (insert "|||||||||||||||||||||||||||||")))
 
-;; default-directory command-line-default-directory
+(progn
+  (set-buffer (other-buffer))
+  (buffer-name)
+  (buffer-size)
+  (save-excursion
+    (point-max)
+    (backward-char 11)
+    (message "%s" (point))
+    (what-cursor-position)))
 
-;; quit-window 函数的使用
-(defun exit-splash-screen ()
-  "Stop displaying the splash screen buffer."
-  (interactive)
-  (quit-window t))
+;; register 相关的应用
+(set-register ?z '(file . "/gd/gnu/emacs/19.0/src/ChangeLog"))
+(number-to-register 10 ?n)
+(increment-register 1 ?n)
 
-(window-width (selected-window))
+;; 环境变量列表 var=val
+(append process-environment)
+(setenv "mvar1" "hhh")
 
-;; 
-(fancy-about-screen)
-(fancy-startup-screen 'concise)
+(let ((buf (set-buffer (read-buffer "buf: "))))
+  (shell-command (read-shell-command "把结果保存到:") buf))
 
-;;;; 软件灵活配置的几种可行方案:
-;; * 使用 key value 形式的简单配置文件
-;; * 发明或者使用现有的脚本引擎，使用脚本来做配置（很多高级功能都可以直接用脚本实现了）
+(message "hello, %s" (char-to-string (read-char "char: ")))
+(buffer-size (set-buffer (read-buffer "buf: ")))
+(completing-read "input: " '("t1" "t2" "t3") nil nil "t" )
 
-;; ./configure
-;; 通过configure，确定相关的系统变量和需求
+;; locole and coding system
+(getenv "LC_CTYPE")
+(getenv "LC_LANG")
+(memq (coding-system-base locale-coding-system)
+      (find-coding-systems-string "你好"))
 
+(coding-system-base locale-coding-system)
+(time-since )
+(let ((str "你好"))
+  (if (multibyte-string-p str)
+      (decode-coding-string str locale-coding-system t)))
 
+(when (eq system-type 'windows-nt)
+  (set-frame-font "courier new"))
 
+;; tmp file directory
+(find-file temporary-file-directory)
 
+;; 批量创建文件 和 编写文件
+(mapc (lambda (n)
+  (let ((fname (concat n ".txt")))
+    (with-current-buffer (get-buffer-create "*hx-test*")
+      (insert (format "%s" (float-time)))
+      (write-file fname)))) '("a" "b" "c" "d"))
 
+(format-time-string "%Y-%m-%d %H:%M")
+
+(setq default-directory "d:/workspace/emacsd/test/")
+(mapc (lambda (f) (delete-file f))
+      (cddr (directory-files default-directory t)))
+
+(message "locale system is: %s" locale-coding-system)
+
+(defun insert-date ()
+    "Insert date at point."
+      (interactive)
+        (insert (format-time-string "%Y-%m-%d - %H:%M")))
+
+;; preloaded-file-list
+;; 预加载的 el 文件
+(setq 'hx-to-preload (reverse preloaded-file-list))
+(find-file-read-only-other-window (buffer-file-name))
